c651021bd393f41417d5387e09b875015faca70b
playdata\playdata2 <gusgh075@gmail.com>
2025-11-22 16:57:27 +0900

    entity : board 생성

--- src/main/java/org/ho/board/domain/Board.java
+++ src/main/java/org/ho/board/domain/Board.java
@@ -1,4 +1,28 @@
 package org.ho.board.domain;
 
-public class Board {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Board  {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+  private String content;
+  private String writer;
+
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
 }
