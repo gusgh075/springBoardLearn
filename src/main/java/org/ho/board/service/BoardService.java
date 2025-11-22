80a0ce0f15bb599ecdd2a2f52f2dbd8ccf32f2f0
playdata\playdata2 <gusgh075@gmail.com>
2025-11-22 16:58:06 +0900

    repo : BoardRepository jpa상속

---
+++ src/main/java/org/ho/board/service/BoardService.java
@@ -0,0 +1,16 @@
+package org.ho.board.service;
+
+import org.ho.board.domain.Board;
+
+public class BoardService {
+  public void saveBoard(Board board){
+  }
+  public void getDetailedBoard(){
+  }
+  public void getListBoard(){
+  }
+  public void updateBoard(){
+  }
+  public void deleteBoard(){
+  }
+}
